a98bf1268b85ef4ac94ef32836389540382359cd
HoneyVanjani <honeyvanjani43@gmail.com>
2026-02-06 11:06:21 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/amul/cattlefeed/CattlefeedApplication.java
@@ -0,0 +1,13 @@
+package com.amul.cattlefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattlefeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattlefeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amul/cattlefeed/CattlefeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amul.cattlefeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CattlefeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
